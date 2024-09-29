fa3c9e938bfc44c3173ff9efc307ee93680018d2
jluissc <jlsc.hco96@gmail.com>
2024-09-28 21:43:01 -0500

    feat: add file list.py with two lines code

--- arrays.py
+++ arrays.py
@@ -2,3 +2,5 @@ print('prueba');
 array = [1,2,3,4,5];
 array2 = [1,2,3,4,5];
 array3 = [1,2,3,4,5];
+
+print(f'hola como van')
---
+++ list.py
@@ -0,0 +1,2 @@
+print('hi angel, how are you?')
+print('i am fine and you?')
